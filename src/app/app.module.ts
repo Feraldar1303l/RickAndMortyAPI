d220134a139f12cef39bb457637029eedd06a7d7
Fer Herrera <generalbicho1303@gmail.com>
2022-12-20 22:21:59 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { NgModuleRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
+import { app_routing } from '../app/app.routes';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { AboutCharacterComponent } from './pages/about-character/about-character.component';
+import { NextPageComponent } from './pages/next-page/next-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    AboutCharacterComponent,
+    NextPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
